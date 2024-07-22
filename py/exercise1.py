3ab68d6c9cf97c51b6f9723450cdf2445a91f17a
yixiangD <yxdeng05@gmail.com>
2024-07-22 14:10:17 -0400

    add exercise2

--- py/exercise1.py
+++ py/exercise1.py
@@ -1,34 +1,36 @@
 import numpy as np
 import pandas as pd
+import sklearn
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression as lm
 # (a)
 df = pd.read_csv("./data/MX_Dengue_trends.csv")
 df["Date"] = pd.to_datetime(df["Date"])
-print(df)
 # plt.show()
 
 # (b)
 df_train = df[:36]
 model = lm()
 model.fit(df_train[["dengue"]], df_train[["Dengue CDC"]])
-print(model.coef_)
-print(model.intercept_)
 
 # (c)
 ymax = 50
 a = model.coef_[0][0]
 b = model.intercept_[0]
 y_fit = a * np.linspace(0, ymax, 20) + b
+
 plt.figure()
 plt.scatter(df_train["dengue"], df_train["Dengue CDC"])
 plt.plot(np.linspace(0, ymax, 20), y_fit, color="r")
-plt.show()
+# plt.show()
 
 # (d)
-df_valid = df[36:]
+df_valid = df[37:]
 y_pred = df_valid[["dengue"]] * a + b
+y_true = df_valid.iloc[:, 2]
+err = sklearn.metrics.mean_squared_error(y_true, y_pred)
+print(err)
 plt.figure()
 plt.scatter(df_valid["dengue"], df_valid["Dengue CDC"])
 plt.plot(df_valid[["dengue"]], y_pred, color="r")
-plt.show()
+# plt.show()
---
+++ py/exercise2.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression as lm
+import sklearn
+# (a)
+df = pd.read_csv("./data/MX_Dengue_trends.csv")
+df["Date"] = pd.to_datetime(df["Date"])
+# print(df)
+# plt.show()
+
+y_pred = []
+model = lm()
+for i in range(len(df) - 37):
+    df_train = df[i:i+36]
+    model.fit(df_train[["dengue"]], df_train[["Dengue CDC"]])
+    a = model.coef_[0][0]
+    b = model.intercept_[0]
+    if i == 0:
+        a0 = a
+        b0 = b
+    x = df.iloc[i+37, 2]
+    y_pred.append(a*x + b)
+    # print(a*x+b, df.iloc[i+37, 1])
+y_true = df.iloc[37:, 1]
+err = sklearn.metrics.mean_squared_error(y_true, y_pred)
+print(err)
+plt.figure()
+plt.plot(df.iloc[37:, 0], df.iloc[37:, 2] * a0 + b0, label="static")
+plt.plot(df.iloc[37:, 0], y_true, label="true")
+plt.plot(df.iloc[37:, 0], y_pred, label="dynamic")
+plt.legend()
+plt.show()
