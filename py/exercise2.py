4ea3ab81acc7fae44d967dfdba454d0c42e343a7
yixiangD <yxdeng05@gmail.com>
2024-07-22 14:32:53 -0400

    update static and dynamic comparison

--- py/exercise2.py
+++ py/exercise2.py
@@ -11,7 +11,7 @@ df["Date"] = pd.to_datetime(df["Date"])
 
 y_pred = []
 model = lm()
-for i in range(len(df) - 37):
+for i in range(len(df) - 36):
     df_train = df[i:i+36]
     model.fit(df_train[["dengue"]], df_train[["Dengue CDC"]])
     a = model.coef_[0][0]
@@ -19,15 +19,16 @@ for i in range(len(df) - 37):
     if i == 0:
         a0 = a
         b0 = b
-    x = df.iloc[i+37, 2]
+    x = df.iloc[i+36, 2]
     y_pred.append(a*x + b)
-    # print(a*x+b, df.iloc[i+37, 1])
-y_true = df.iloc[37:, 1]
-err = sklearn.metrics.mean_squared_error(y_true, y_pred)
-print(err)
+    # print(a*x+b, df.iloc[i+36, 1])
+y_true = df.iloc[36:, 1]
+err_dyn = sklearn.metrics.mean_squared_error(y_true, y_pred)
+err_stat = sklearn.metrics.mean_squared_error(y_true, df.iloc[36:, 2] * a0 + b0)
+print(err_dyn, err_stat)
 plt.figure()
-plt.plot(df.iloc[37:, 0], df.iloc[37:, 2] * a0 + b0, label="static")
-plt.plot(df.iloc[37:, 0], y_true, label="true")
-plt.plot(df.iloc[37:, 0], y_pred, label="dynamic")
+plt.plot(df.iloc[36:, 0], df.iloc[36:, 2] * a0 + b0, label="static")
+plt.plot(df.iloc[36:, 0], y_true, label="true")
+plt.plot(df.iloc[36:, 0], y_pred, label="dynamic")
 plt.legend()
 plt.show()
